392cf96d5f121e33601adea90a3681d3637502b3
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2025-11-25 20:17:24 +0200

    add CarouselComponent SEO-friendly

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
             <section aria-label="top screen" className="xl:max-w-7xl lg:max-w-5xl m-auto flex items-end overflow-hidden px-4 relative">
                 <Sidebar aria-label="Product Categories Navigation" />
 
-                <div className="flex-1 h-full md:ps-7 overflow-hidden">
+                <div className="flex-1 h-full md:ps-7 pe-1 overflow-hidden">
                     <CarouselComponent />
                 </div>
             </section>
