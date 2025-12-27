1af70eb17340ae3067971d6759c69904260ae6d6
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2025-12-27 21:47:31 +0200

    add products page

--- src/app/(routes)/products/page.tsx
+++ src/app/(routes)/products/page.tsx
@@ -1,7 +1,10 @@
+import Breadcrumb from '@/src/components/Breadcrumb/Breadcrumb'
 import React from 'react'
 
 export default function page() {
     return (
-        <div>page</div>
+        <main className="xl:max-w-7xl lg:max-w-5xl m-auto px-4 py-8">
+            <Breadcrumb items={[{ label: 'Products' }]} />
+        </main>
     )
 }
