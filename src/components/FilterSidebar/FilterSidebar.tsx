8046059fa6e4ddfbffcc58c12c1bac995d021cce
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2025-12-27 22:02:03 +0200

    add button to close FilterSideba

--- src/components/FilterSidebar/FilterSidebar.tsx
+++ src/components/FilterSidebar/FilterSidebar.tsx
@@ -15,6 +15,27 @@ export default function FilterSidebar() {
                 </svg>
                 Filters
             </button>
+
+            {/* Sidebar */}
+            <aside className={`
+                    fixed lg:static top-0 left-0 h-full lg:h-auto w-80 lg:w-64
+                    bg-white lg:bg-transparent z-50 lg:z-auto
+                    transform lg:transform-none transition-transform duration-300
+                    ${isOpen ? 'translate-x-0' : '-translate-x-full lg:translate-x-0'}
+                    overflow-y-auto p-6 lg:p-0 border-r lg:border-r-0 border-borderColor
+                `}
+            >
+                {/* Mobile Close Button */}
+                <button
+                    onClick={() => setIsOpen(false)}
+                    className="lg:hidden absolute top-4 right-4 w-8 h-8 flex items-center justify-center"
+                    aria-label="Close filters"
+                >
+                    <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
+                        <path d="M18 6L6 18M6 6l12 12" />
+                    </svg>
+                </button>
+            </aside>
         </>
     )
 }
