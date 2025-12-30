f16137835ddee3c9be600e16800747e3016d37b0
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2025-12-30 21:31:51 +0200

    add FilterSidebar to products page

--- src/app/(routes)/products/page.tsx
+++ src/app/(routes)/products/page.tsx
@@ -1,15 +1,21 @@
+"use client"
 import Breadcrumb from '@/src/components/Breadcrumb/Breadcrumb'
-import FilterSidebar from '@/src/components/FilterSidebar/FilterSidebar'
-import React from 'react'
+import FilterSidebar, { FilterState } from '@/src/components/FilterSidebar/FilterSidebar'
+import React, { useState } from 'react'
 
 export default function page() {
+    const [filters, setFilters] = useState<FilterState>({
+        categories: [],
+        priceRange: [0, 1000],
+        rating: 0,
+    });
     return (
         <main className="xl:max-w-7xl lg:max-w-5xl m-auto px-4 py-8">
             <Breadcrumb items={[{ label: 'Products' }]} />
 
             <div className="flex flex-col lg:flex-row gap-8">
                 {/* Sidebar */}
-                <FilterSidebar />
+                <FilterSidebar onFilterChange={setFilters} />
             </div>
         </main>
     )
