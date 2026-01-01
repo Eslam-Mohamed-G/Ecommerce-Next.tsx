cfe0bd4a125665ddc8f7d212d777e9544af9337c
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2026-01-01 20:58:36 +0200

    add Action Buttons to prodcut card

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -31,11 +31,13 @@ export default function ProductCard({ id, title, price, originalPrice, discount,
                     )}
                 </div>
 
+                {/* Action Buttons */}
                 <div className="flex flex-col gap-2 items-center justify-center absolute top-3 right-3">
-                    <button type='button' aria-label="Add to favorites" className='w-8 h-8 rounded-full bg-white flex items-center justify-center text-textColor cursor-pointer'>
+                    <button type='button' aria-label="Add to favorites" className='w-8 h-8 bg-white rounded-full flex items-center justify-center hover:bg-primaryColor hover:text-white transition-colors ease-in-out duration-300 cursor-pointer'>
                         <svg aria-hidden="true" width={22} height={22} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-heart-icon lucide-heart"><path d="M2 9.5a5.5 5.5 0 0 1 9.591-3.676.56.56 0 0 0 .818 0A5.49 5.49 0 0 1 22 9.5c0 2.29-1.5 4-3 5.5l-5.492 5.313a2 2 0 0 1-3 .019L5 15c-1.5-1.5-3-3.2-3-5.5" /></svg>
                     </button>
-                    <button type='button' aria-label="View details" className='w-8 h-8 rounded-full bg-white flex items-center justify-center text-textColor cursor-pointer'>
+
+                    <button type='button' aria-label="View details" className='w-8 h-8 bg-white rounded-full flex items-center justify-center hover:bg-primaryColor hover:text-white transition-colors ease-in-out duration-300 cursor-pointer'>
                         <svg aria-hidden="true" width={22} height={22} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-eye-icon lucide-eye"><path d="M2.062 12.348a1 1 0 0 1 0-.696 10.75 10.75 0 0 1 19.876 0 1 1 0 0 1 0 .696 10.75 10.75 0 0 1-19.876 0" /><circle cx={12} cy={12} r={3} /></svg>
                     </button>
                 </div>
