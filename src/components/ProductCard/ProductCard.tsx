f343b68ec461870c619e366d4132f3cdbfcfba10
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2026-01-10 20:21:30 +0200

    handle products style

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -42,7 +42,7 @@ export default function ProductCard({ id, title, price, priceAfterDiscount, imag
 
     const displayPrice = priceAfterDiscount || price;
     return (
-        <div role="article" className='w-3xs group'>
+        <div role="article" className='w-full md:w-56 group'>
             <div className="bg-primaryBackground rounded flex items-center justify-center p-6 overflow-hidden relative">
                 {/* Badges */}
                 <div className="absolute top-3 left-3 z-10 flex flex-col gap-2">
