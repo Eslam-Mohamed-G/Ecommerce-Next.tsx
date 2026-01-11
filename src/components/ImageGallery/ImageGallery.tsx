f8d571dde68e20932035bf3abd2cb87fb36052bb
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2026-01-11 20:17:05 +0200

    add main image to ImageGallery component

--- src/components/ImageGallery/ImageGallery.tsx
+++ src/components/ImageGallery/ImageGallery.tsx
@@ -14,19 +14,21 @@ export default function ImageGallery({ images, productName }: ImageGalleryProps)
         <div className="flex flex-col gap-4">
             {/* main Image */}
             <div className="relative bg-primaryBackground rounded-lg overflow-hidden group cursor-zoom-in" onClick={() => setIsZoomed(!isZoomed)}>
-                <Image
-                    src={images[selectedImage]}
-                    alt={`${productName} - Image ${selectedImage + 1}`}
-                    fill
-                    className="object-contain p-8"
-                    priority
-                />
-            </div>
-            {isZoomed && (
-                <div className="absolute top-4 right-4 bg-white/90 px-3 py-1 rounded text-sm">
-                    Click to zoom out
+                <div className={`relative w-full h-[400px] md:h-[500px] transition-transform duration-300 ${isZoomed ? 'scale-150' : 'scale-100'}`}>
+                    <Image
+                        src={images[selectedImage]}
+                        alt={`${productName} - Image ${selectedImage + 1}`}
+                        fill
+                        className="object-contain p-8"
+                        priority
+                    />
                 </div>
-            )}
+                {isZoomed && (
+                    <div className="absolute top-4 right-4 bg-white/90 px-3 py-1 rounded text-sm">
+                        Click to zoom out
+                    </div>
+                )}
+            </div>
         </div>
     )
 }
