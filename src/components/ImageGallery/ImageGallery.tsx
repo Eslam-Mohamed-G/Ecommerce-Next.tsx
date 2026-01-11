5b43d22b0b2182a90663836adbf6944f64bba905
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2026-01-11 20:15:04 +0200

    add image to ImageGallery component

--- src/components/ImageGallery/ImageGallery.tsx
+++ src/components/ImageGallery/ImageGallery.tsx
@@ -13,7 +13,7 @@ export default function ImageGallery({ images, productName }: ImageGalleryProps)
     return (
         <div className="flex flex-col gap-4">
             {/* main Image */}
-            <div className="relative bg-primaryBackground rounded-lg overflow-hidden group cursor-zoom-in">
+            <div className="relative bg-primaryBackground rounded-lg overflow-hidden group cursor-zoom-in" onClick={() => setIsZoomed(!isZoomed)}>
                 <Image
                     src={images[selectedImage]}
                     alt={`${productName} - Image ${selectedImage + 1}`}
@@ -22,6 +22,11 @@ export default function ImageGallery({ images, productName }: ImageGalleryProps)
                     priority
                 />
             </div>
+            {isZoomed && (
+                <div className="absolute top-4 right-4 bg-white/90 px-3 py-1 rounded text-sm">
+                    Click to zoom out
+                </div>
+            )}
         </div>
     )
 }
