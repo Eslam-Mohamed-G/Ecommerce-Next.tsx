7b04ad9ff71c64c66af1a588e7c7913798d64504
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2026-01-11 15:41:53 +0200

    add Breadcrumb components

--- src/app/(routes)/products/[id]/page.tsx
+++ src/app/(routes)/products/[id]/page.tsx
@@ -1,8 +1,16 @@
 "use client"
+import Breadcrumb from '@/src/components/Breadcrumb/Breadcrumb'
 import React from 'react'
 
 export default function ProductDetailsPage() {
     return (
-        <div>ProductDetailsPage</div>
+        <section className='xl:max-w-7xl lg:max-w-5xl m-auto px-4 py-8'>
+            <Breadcrumb
+                items={[
+                    { label: 'Products', href: '/products' },
+                    { label: 'mockProduct.name' },
+                ]}
+            />
+        </section>
     )
 }
