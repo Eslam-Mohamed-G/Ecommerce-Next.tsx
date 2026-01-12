b594447dbe539282d5c82d652cfcc840ac33426f
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2026-01-12 20:30:00 +0200

    add interface and props to QuantitySelector component

--- src/components/QuantitySelector/QuantitySelector.tsx
+++ src/components/QuantitySelector/QuantitySelector.tsx
@@ -1,7 +1,21 @@
 "use client";
 import React from 'react';
 
-export default function QuantitySelector() {
+interface QuantitySelectorProps {
+    quantity: number;
+    onIncrease: () => void;
+    onDecrease: () => void;
+    min?: number;
+    max?: number;
+}
+
+export default function QuantitySelector({
+    quantity,
+    onIncrease,
+    onDecrease,
+    min = 1,
+    max = 99
+}: QuantitySelectorProps) {
     return (
         <div>QuantitySelector</div>
     )
