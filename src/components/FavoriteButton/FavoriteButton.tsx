aa7077fe406f383dc39a8e732ce1fef0b85307f5
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2026-01-13 19:49:00 +0200

    add FavoriteButton component

---
+++ src/components/FavoriteButton/FavoriteButton.tsx
@@ -0,0 +1,9 @@
+"use client"
+import React, { useState } from 'react';
+
+export default function FavoriteButton() {
+    const [state, setstate] = useState();
+    return (
+        <div>FavoriteButton</div>
+    )
+};
