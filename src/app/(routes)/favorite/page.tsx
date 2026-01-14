bb70311c7e141ae40226a127b15534d093029dd7
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2026-01-14 20:22:21 +0200

    add favorite page

---
+++ src/app/(routes)/favorite/page.tsx
@@ -0,0 +1,15 @@
+"use client"
+import React, { useEffect, useState } from 'react';
+
+export default function page() {
+    const [state, setstate] = useState();
+    useEffect(() => {
+
+        return () => {
+
+        };
+    }, []);
+    return (
+        <div>page</div>
+    )
+}
