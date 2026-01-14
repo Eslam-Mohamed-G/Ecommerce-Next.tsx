62b98af70a7b0ec51f1d1fa33299da716eb030bc
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2026-01-14 20:28:09 +0200

    add useState to handle the loading state while fetching data , stores any error message returned from the request

--- src/app/(routes)/favorite/page.tsx
+++ src/app/(routes)/favorite/page.tsx
@@ -1,10 +1,13 @@
 "use client"
+import { Product } from '@/src/components/ProductCard/ProductCard';
 import React, { useEffect, useState } from 'react';
 
 export default function page() {
-    const [state, setstate] = useState();
+    const [wishList, setWishList] = useState<Product[]>([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState<string | null>(null);
     useEffect(() => {
-
+        
         return () => {
 
         };
