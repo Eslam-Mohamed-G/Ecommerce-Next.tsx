32e1a3f9de431233facb9a6920ca34b651381787
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2026-01-14 20:39:46 +0200

    add token

--- src/app/(routes)/favorite/page.tsx
+++ src/app/(routes)/favorite/page.tsx
@@ -1,14 +1,21 @@
 "use client"
 import { Product } from '@/src/components/ProductCard/ProductCard';
+import { getCookie } from 'cookies-next';
 import React, { useEffect, useState } from 'react';
 
 export default function page() {
     const [wishList, setWishList] = useState<Product[]>([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState<string | null>(null);
+
+    // useEffect will be used to fetch the wishlist data when the page loads.
     useEffect(() => {
         const getUserWishlist = async () => {
-            
+            const token = getCookie("token") as string | undefined;
+            if (!token) {
+                alert("You are not logged in");
+                return;
+            }
         }
         return () => {
 
