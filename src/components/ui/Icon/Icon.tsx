27888d388de117dbbbb810c430127d0271ebebf3
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2026-01-18 21:19:48 +0200

    add interface to Icon components library

--- src/components/ui/Icon/Icon.tsx
+++ src/components/ui/Icon/Icon.tsx
@@ -1,4 +1,9 @@
-import React from 'react'
+import React from 'react';
+interface IconProps {
+    className?: string;
+    width?: number;
+    height?: number;
+}
 
 export default function Icon() {
     return (
