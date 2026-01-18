82872ba69388f063c17e159ced633dd8e01bd474
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2026-01-18 21:20:31 +0200

    add EyeIcon to Icon components

--- src/components/ui/Icon/Icon.tsx
+++ src/components/ui/Icon/Icon.tsx
@@ -5,8 +5,20 @@ interface IconProps {
     height?: number;
 }
 
-export default function Icon() {
-    return (
-        <div>Icon</div>
-    )
-}
+export const EyeIcon: React.FC<IconProps> = ({ className = "", width = 22, height = 22 }) => (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width={width}
+        height={height}
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth={2}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        className={className}
+    >
+        <path d="M2.062 12.348a1 1 0 0 1 0-.696 10.75 10.75 0 0 1 19.876 0 1 1 0 0 1 0 .696 10.75 10.75 0 0 1-19.876 0" />
+        <circle cx={12} cy={12} r={3} />
+    </svg>
+);
