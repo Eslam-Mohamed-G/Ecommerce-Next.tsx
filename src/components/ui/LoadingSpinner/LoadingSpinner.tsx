77397913e1f5aa6759340375dc63a2d42df5a900
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2026-01-18 21:30:39 +0200

    add interface to LoadingSpinner

--- src/components/ui/LoadingSpinner/LoadingSpinner.tsx
+++ src/components/ui/LoadingSpinner/LoadingSpinner.tsx
@@ -1,5 +1,8 @@
-import React from 'react'
-
+import React from 'react';
+interface LoadingSpinnerProps {
+    size?: 'sm' | 'md' | 'lg';
+    className?: string;
+}
 export default function LoadingSpinner() {
   return (
     <div>LoadingSpinner</div>
