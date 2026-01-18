1cd9d24a8a438b48b0fa2760887595f1cf0091c4
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2026-01-18 21:27:57 +0200

    add TrashIcon to Icon components

--- src/components/FavoriteButton/FavoriteButton.tsx
+++ src/components/FavoriteButton/FavoriteButton.tsx
@@ -3,7 +3,7 @@ import { getCookie } from "cookies-next";
 import { usePathname } from 'next/navigation';
 import React, { useState } from 'react';
 import ToastMessage from "../ToastMessage/ToastMessage";
-import { HeartIcon } from "../ui/Icon/Icon";
+import { HeartIcon, TrashIcon } from "../ui/Icon/Icon";
 
 interface favoriteButtonProps {
     cssStyle: string;
@@ -135,7 +135,8 @@ export default function FavoriteButton({ cssStyle, product_Id }: favoriteButtonP
                             <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
                         </svg>
                     ) : (
-                        <svg aria-hidden="true" className="lucide lucide-trash2-icon lucide-trash-2" width={22} height={22} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round"><path d="M10 11v6" /><path d="M14 11v6" /><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6" /><path d="M3 6h18" /><path d="M8 6V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2" /></svg>
+                        <TrashIcon />
+                        // <svg aria-hidden="true" className="lucide lucide-trash2-icon lucide-trash-2" width={22} height={22} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round"><path d="M10 11v6" /><path d="M14 11v6" /><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6" /><path d="M3 6h18" /><path d="M8 6V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2" /></svg>
                     )}
                 </button>
                 :
--- src/components/ui/Icon/Icon.tsx
+++ src/components/ui/Icon/Icon.tsx
@@ -60,3 +60,25 @@ export const HeartIcon: React.FC<IconProps> = ({ className = "", width = 24, hei
         <path d="M2 9.5a5.5 5.5 0 0 1 9.591-3.676.56.56 0 0 0 .818 0A5.49 5.49 0 0 1 22 9.5c0 2.29-1.5 4-3 5.5l-5.492 5.313a2 2 0 0 1-3 .019L5 15c-1.5-1.5-3-3.2-3-5.5" />
     </svg>
 );
+
+export const TrashIcon: React.FC<IconProps> = ({ className = "", width = 22, height = 22 }) => (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width={width}
+        height={height}
+        aria-hidden="true"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth={2}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        className={className}
+    >
+        <path d="M10 11v6" />
+        <path d="M14 11v6" />
+        <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6" />
+        <path d="M3 6h18" />
+        <path d="M8 6V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2" />
+    </svg>
+);
