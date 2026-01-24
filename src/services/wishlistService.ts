509a780cb9917cdaa7406df633a7826737475c4b
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2026-01-24 21:52:36 +0200

    add Get user's wishlist

--- src/services/wishlistService.ts
+++ src/services/wishlistService.ts
@@ -1,3 +1,12 @@
 import apiClient from './apiClient';
 import { API_ENDPOINTS } from './endpoints';
 import { Product, ApiResponse } from '../types';
+
+
+/**
+ * Get user's wishlist
+ */
+export const getWishlist =async (): Promise<ApiResponse<Product[]>> => {
+    const response = await apiClient.get(API_ENDPOINTS.WISHLIST.BASE);
+    return response.data;
+}
