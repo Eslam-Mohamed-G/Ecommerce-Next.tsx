96c7ef8e79d93253a2006af99015586a82ddb8a8
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2026-01-26 16:30:09 +0200

    add toast type

--- src/context/ToastContext.tsx
+++ src/context/ToastContext.tsx
@@ -1,18 +1,19 @@
 "use client";
-import React, { createContext, useState } from 'react';
+import React, { createContext, useEffect, useState } from 'react';
 
+export type ToastType = "success" | "error" | "warning" | "info";
 export interface ToastContextType {
-    type: "success" | "error" | "warning" | "info";
+    type: ToastType;
     message: string;
     duration?: number;
-    showToast: void;
+    showToast: boolean;
     removeToast: void;
 }
 
 const ToastContext = createContext<ToastContextType | null>(null);
 
 export const ToastProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-    const [state, setstate] = useState();
+
     return (
         <ToastContext.Provider value={ }>
             {children}
