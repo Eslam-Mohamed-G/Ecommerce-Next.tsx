4c28cf5d6712f387b7a8d8f9abe5a89127e131dd
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2026-01-26 20:02:22 +0200

    add getUserWishlist fun to get user wishlist

--- src/context/WishlistContext.tsx
+++ src/context/WishlistContext.tsx
@@ -1,6 +1,8 @@
 "use client";
 import React, { ReactNode, createContext, useContext, useEffect, useState } from 'react';
 import { Product } from '../types';
+import { getCookie } from 'cookies-next';
+import wishlistService from '../services/wishlistService';
 
 export interface WishlistContextType {
     wishlist: Product[];
@@ -13,13 +15,28 @@ export const WishlistProvider = ({ children }: { children: ReactNode }) => {
     const [wishlist, setWishlist] = useState<Product[]>([]);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState<string | null>(null);
-    
-    useEffect(() => {
-        
-        return () => {
-            
-        };
-    }, []);
+
+    const getUserWishlist = async () => {
+        const token = getCookie("token") as string | undefined;
+        if (!token) {
+            alert("You are not logged in");
+            return;
+        }
+
+        setLoading(true);
+        setError("");
+        try {
+            const response = await wishlistService.getWishlist();
+            if (response.data) {
+                setWishlist(response.data);
+            }
+        } catch (error) {
+            setError(error instanceof Error ? error.message : 'An error occurred');
+        } finally {
+            setLoading(false);
+        }
+    };
+
     return (
         <WishlistContext.Provider value={{}}>
             {children}
