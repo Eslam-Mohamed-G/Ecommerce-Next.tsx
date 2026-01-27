17a48598060a405b3fab9d9a2a520525e6372731
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2026-01-27 12:09:47 +0200

    add getUserWishlist

--- src/components/features/FavoriteButton/FavoriteButton.tsx
+++ src/components/features/FavoriteButton/FavoriteButton.tsx
@@ -6,6 +6,7 @@ import LoadingSpinner from "../../ui/LoadingSpinner/LoadingSpinner";
 import wishlistService from '@/src/services/wishlistService';
 import { useToast } from '@/src/context/ToastContext';
 import { getCookie } from 'cookies-next';
+import { useWishlist } from '@/src/context/WishlistContext';
 
 interface favoriteButtonProps {
     cssStyle: string;
@@ -17,6 +18,7 @@ export default function FavoriteButton({ cssStyle, product_Id }: favoriteButtonP
     const isWishlistPage = pathname === "/wishList";
     const [loading, setLoading] = useState(false);
     const { showToast } = useToast();
+    const { getUserWishlist } = useWishlist();
 
     const handleWishlistAction = async (action: 'add' | 'remove') => {
         if (!product_Id) {
@@ -50,6 +52,7 @@ export default function FavoriteButton({ cssStyle, product_Id }: favoriteButtonP
             showToast("error", "There's an error. Please try again.");
         } finally {
             setLoading(false);
+            getUserWishlist();
         };
     };
 
