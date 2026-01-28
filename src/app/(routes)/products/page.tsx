2b5de7072d0b5fd83a1dd8dd102fb69679c84468
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2026-01-28 16:59:09 +0200

    add ProductCardProps

--- src/app/(routes)/products/page.tsx
+++ src/app/(routes)/products/page.tsx
@@ -136,7 +136,7 @@ export default function page() {
                             {filteredProducts.length > 0 ? (
                                 <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-6">
                                     {filteredProducts.map((product) => (
-                                        <ProductCard key={product.id} {...product} />
+                                        <ProductCard key={product.id} className='w-full md:w-56 group' {...product} />
                                     ))}
                                 </div>
                             ) : (
