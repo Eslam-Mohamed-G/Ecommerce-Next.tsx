93a989160fcc959a4fb17ab76ed418cb6016cdd1
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2026-01-29 12:49:35 +0200

    add to cart button

---
+++ src/components/features/AddToCartButton/AddToCartButton.tsx
@@ -0,0 +1,42 @@
+"use client"
+import { useToast } from '@/src/context/ToastContext';
+import cartService from '@/src/services/cartService';
+import { getCookie } from 'cookies-next';
+import React, { useState } from 'react';
+
+interface AddToCartButtonProps {
+    className: string;
+    product_Id: string;
+};
+
+export default function AddToCartButton({ className, product_Id }: AddToCartButtonProps) {
+    const [loading, setLoading] = useState(false);
+    const { showToast } = useToast();
+
+    const addToCart = async (product_Id: string) => {
+        if (!product_Id) {
+            showToast("warning", "There's an error. Please try again.");
+            return;
+        }
+
+        const token = getCookie("token");
+        if (!token) {
+            showToast("warning", "You are not logged in");
+            return;
+        };
+
+        try {
+            setLoading(true);
+            await cartService.addToCart(product_Id);
+            showToast("success", "Added to your cart");
+        } catch (error) {
+            showToast("error", "There's an error. Please try again.");
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    return (
+        <button type='button' aria-label='add product to cart' onClick={()=> addToCart(product_Id)} className={className}>Add To Cart</button>
+    )
+};
