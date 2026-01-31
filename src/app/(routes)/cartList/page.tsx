8d9040f6ca34d74d0b168cb9d7aa91b468e648a3
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2026-01-31 22:19:53 +0200

    add cartList page

---
+++ src/app/(routes)/cartList/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+import { useGetProducts } from '@/src/context/GetProductsContext';
+import React from 'react';
+
+export default function page() {
+    const { cartlistLoading, error, cartList, getUserCart } = useGetProducts();
+    return (
+        <div>page</div>
+    )
+};
