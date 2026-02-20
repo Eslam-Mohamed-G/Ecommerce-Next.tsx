f8a44bdfa10058c14f81216bf546100f53b215f8
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2026-02-20 22:28:25 +0200

    to handle error state

--- src/components/ForgotPasswordModal/ForgotPasswordModal.tsx
+++ src/components/ForgotPasswordModal/ForgotPasswordModal.tsx
@@ -11,6 +11,7 @@ type Step = 1 | 2 | 3;
 export default function ForgotPasswordModal({ isOpen, onClose }: ForgotPasswordModalProps) {
     const [step, setStep] = useState<Step>(1);
     const [loading, setLoading] = useState(false);
+    const [globalError, setGlobalError] = useState("");
     return (
         <div>ForgotPasswordModal</div>
     )
