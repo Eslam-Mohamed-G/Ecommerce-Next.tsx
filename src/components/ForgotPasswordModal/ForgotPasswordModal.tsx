6a6cd3d11c3c1c6e5076a54398d534ac42390a7c
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2026-02-21 13:09:26 +0200

    handle close the model

--- src/components/ForgotPasswordModal/ForgotPasswordModal.tsx
+++ src/components/ForgotPasswordModal/ForgotPasswordModal.tsx
@@ -21,11 +21,12 @@ export default function ForgotPasswordModal({ isOpen, onClose }: ForgotPasswordM
         setGlobalSuccess("");
         onClose();
     };
+    if (!isOpen) return null;
     
     return (
         <div className="fixed inset-0 z-50 flex items-center justify-center p-4 bg-black/50 backdrop-blur-sm transition-opacity ease-in-out duration-300">
             <div className="bg-white rounded-lg shadow-xl w-full max-w-md overflow-hidden relative">
-                
+
             </div>
         </div>
     )
