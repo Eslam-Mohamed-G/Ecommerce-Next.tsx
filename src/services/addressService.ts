90c1c3c353fe9500aaa9be2bd4f3967c4d3d2524
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2026-02-22 14:35:28 +0200

    add Address Service to Handles all address-related API calls

---
+++ src/services/addressService.ts
@@ -0,0 +1,8 @@
+import apiClient from './apiClient';
+import { API_ENDPOINTS } from './endpoints';
+import { ApiResponse } from '../types';
+
+/**
+ * Address Service
+ * Handles all address-related API calls
+*/
