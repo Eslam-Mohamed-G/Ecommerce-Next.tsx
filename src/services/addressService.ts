3c411f76cb962111599c9f5ebb3e117ecb528693
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2026-02-22 14:40:19 +0200

    add new Address request

--- src/services/addressService.ts
+++ src/services/addressService.ts
@@ -1,8 +1,16 @@
 import apiClient from './apiClient';
 import { API_ENDPOINTS } from './endpoints';
-import { ApiResponse } from '../types';
+import { ApiResponse, Address } from '../types';
 
 /**
  * Address Service
  * Handles all address-related API calls
 */
+
+/**
+ * Add new address
+*/
+export const addAddress = async (addressData: Address): Promise<ApiResponse<Address>> => {
+    const response = await apiClient.post(API_ENDPOINTS.ADDRESSES.BASE, addressData);
+    return response.data;
+};
