1af6f712297c94ce6cab86a108601e4de8233c3d
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2026-02-24 16:48:35 +0200

    add wishlist

--- src/app/(routes)/wishlist/page.tsx
+++ src/app/(routes)/wishlist/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import ProductCard from '@/src/components/features/ProductCard/ProductCard';
 import { useGetProducts } from '@/src/context/GetProductsContext';
 import React, { useEffect } from 'react';
