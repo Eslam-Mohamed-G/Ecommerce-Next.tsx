6f34f27250e765b13a24744b514579e32be622f7
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2026-02-26 13:03:34 +0200

    add css style to wishlist card

--- src/app/(routes)/wishlist/page.tsx
+++ src/app/(routes)/wishlist/page.tsx
@@ -53,7 +53,7 @@ export default function page() {
                     {wishlist.length > 0 ? (
                         <div className="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-6">
                             {wishlist.map((product) => (
-                                <ProductCard key={product.id} {...product} />
+                                <ProductCard key={product.id} {...product} className='animate-zoom-in' />
                             ))}
                         </div>
                     ) : (
