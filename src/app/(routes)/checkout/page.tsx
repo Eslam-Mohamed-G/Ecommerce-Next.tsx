e619db27a2d5f47925ca583adcb28cdccf6b6f49
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2026-03-09 16:52:59 +0200

    handle cart items from context

--- src/app/(routes)/checkout/page.tsx
+++ src/app/(routes)/checkout/page.tsx
@@ -1,7 +1,18 @@
+"use client";
 import Breadcrumb from '@/src/components/common/Breadcrumb/Breadcrumb';
-import React from 'react';
+import { useGetProducts } from '@/src/context/GetProductsContext';
+import React, { useEffect } from 'react';
 
 export default function page() {
+    const { cartlistLoading, cartError, cartList, getUserCart } = useGetProducts();
+
+    useEffect(() => {
+        getUserCart();
+        return () => {
+
+        };
+    }, []);
+    
     return (
         <section className="w-full min-h-96 lg:max-w-5xl xl:max-w-7xl m-auto px-4 py-8">
             <Breadcrumb items={[{ label: 'Cart', href: '/cart' }, { label: 'Checkout' }]} />
