01ec14fb140e868af7096e789416e82bb3249c7d
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2026-03-09 12:46:36 +0200

    add Order Service --> Handles all order-related API calls

---
+++ src/services/orderService.ts
@@ -0,0 +1,13 @@
+import apiClient from './apiClient';
+import { API_ENDPOINTS } from './endpoints';
+import { Order, ShippingAddress, ApiResponse } from '../types';
+
+/**
+ * Order Service --> Handles all order-related API calls
+*/
+
+const orderService = {
+
+};
+
+export default orderService;
