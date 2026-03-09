e400051d5f7f69567fa637ffc33d453c148c5967
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2026-03-09 16:41:26 +0200

    add Billing Details form

--- src/app/(routes)/checkout/page.tsx
+++ src/app/(routes)/checkout/page.tsx
@@ -5,6 +5,95 @@ export default function page() {
     return (
         <section className="w-full min-h-96 lg:max-w-5xl xl:max-w-7xl m-auto px-4 py-8">
             <Breadcrumb items={[{ label: 'Cart', href: '/cart' }, { label: 'Checkout' }]} />
+
+            <form action="" className="grid grid-cols-1 lg:grid-cols-3 gap-8">
+                {/* Billing Details */}
+                <div className="lg:col-span-2">
+                    <h2 className="text-xl font-semibold mb-6">Billing Details</h2>
+                    <div className="grid grid-cols-1 gap-6">
+                        {/* Name */}
+                        <div>
+                            <label htmlFor="name" className="block text-sm font-medium mb-1">
+                                Name <span className="text-primaryColor">*</span>
+                            </label>
+                            <input
+                                type="text"
+                                id="name"
+                                name="name"
+                                className={`w-full px-4 py-3 border rounded focus:outline-none focus:border-primaryColor`}
+                            />
+                        </div>
+
+                        {/* Email */}
+                        <div>
+                            <label htmlFor="email" className="block text-sm font-medium mb-1">
+                                Email <span className="text-primaryColor">*</span>
+                            </label>
+                            <input
+                                type="email"
+                                id="email"
+                                name="email"
+                                className={`w-full px-4 py-3 border rounded focus:outline-none focus:border-primaryColor`}
+                            />
+                        </div>
+
+                        {/* Phone */}
+                        <div>
+                            <label htmlFor="phone" className="block text-sm font-medium mb-1">
+                                Phone <span className="text-primaryColor">*</span>
+                            </label>
+                            <input
+                                type="tel"
+                                id="phone"
+                                name="phone"
+                                className={`w-full px-4 py-3 border rounded focus:outline-none focus:border-primaryColor`}
+                            />
+                        </div>
+
+                        {/* Address */}
+                        <div className="">
+                            <label htmlFor="address" className="block text-sm font-medium mb-1">
+                                Street Address <span className="text-primaryColor">*</span>
+                            </label>
+                            <input
+                                type="text"
+                                id="address"
+                                name="address"
+                                className={`w-full px-4 py-3 border rounded focus:outline-none focus:border-primaryColor`}
+                            />
+                        </div>
+
+                        {/* City */}
+                        <div>
+                            <label htmlFor="city" className="block text-sm font-medium mb-1">
+                                City <span className="text-primaryColor">*</span>
+                            </label>
+                            <input
+                                type="text"
+                                id="city"
+                                name="city"
+                                className={`w-full px-4 py-3 border rounded focus:outline-none focus:border-primaryColor`}
+                            />
+                        </div>
+                        
+                        {/* ZIP Code */}
+                        <div>
+                            <label htmlFor="zipCode" className="block text-sm font-medium mb-2">
+                                ZIP Code <span className="text-primaryColor">*</span>
+                            </label>
+                            <input
+                                type="text"
+                                id="zipCode"
+                                name="zipCode"
+                                className={`w-full px-4 py-3 border rounded focus:outline-none focus:border-primaryColor`}
+                            />
+                        </div>
+                    </div>
+                </div>
+
+                {/* Order Summary */}
+                <div className="lg:col-span-1"></div>
+            </form>
         </section>
     )
 };
