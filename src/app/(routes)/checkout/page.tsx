cebc6630a924c97d1eaf9c52def874a3dc09efdc
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2026-03-12 14:35:25 +0200

    add card expiry input

--- src/app/(routes)/checkout/page.tsx
+++ src/app/(routes)/checkout/page.tsx
@@ -246,7 +246,15 @@ export default function page() {
                                     className="w-full px-4 py-2 border border-borderColor rounded focus:outline-none focus:border-primaryColor"
                                 />
                                 <div className="grid grid-cols-2 gap-3">
-
+                                    {/* card expiry */}
+                                    <input
+                                        type="text"
+                                        name="cardExpiry"
+                                        placeholder="MM/YY"
+                                        value={formik.values.cardExpiry}
+                                        onChange={formik.handleChange}
+                                        className="px-4 py-2 border border-borderColor rounded focus:outline-none focus:border-primaryColor"
+                                    />
                                 </div>
                             </div>
                         )}
