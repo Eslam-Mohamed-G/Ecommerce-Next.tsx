87ac080957615d17980885855d3eaa7e2cb3192e
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2026-03-12 13:53:43 +0200

    handle payment LoadingSpinner

--- src/app/(routes)/checkout/page.tsx
+++ src/app/(routes)/checkout/page.tsx
@@ -237,7 +237,7 @@ export default function page() {
                             disabled={paymentLoading || cartlistLoading || !cartList?.products?.length}
                             className="w-full flex items-center justify-center gap-2 py-3 bg-primaryColor text-white font-semibold rounded hover:opacity-90 transition-opacity ease-in-out duration-300 disabled:opacity-50 cursor-pointer disabled:cursor-not-allowed"
                         >
-                            {paymentLoading && <LoadingSpinner size="sm" className='text-white'/>}
+                            {paymentLoading && <LoadingSpinner size="sm" className='text-white' />}
                             {formik.values.paymentMethod === 'card' ? 'Pay Online' : 'Place Order'}
                         </button>
                     </form>
