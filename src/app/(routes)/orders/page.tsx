f56a22793307f3679e16adaced033991d2aab274
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2026-03-13 17:47:28 +0200

    add order page

---
+++ src/app/(routes)/orders/page.tsx
@@ -0,0 +1,18 @@
+"use client";
+import React, { useEffect, useState } from 'react';
+
+export default function page() {
+    const [state, setstate] = useState();
+    useEffect(() => {
+
+        return () => {
+
+        };
+    }, []);
+    
+    return (
+        <section className="w-full min-h-96 lg:max-w-5xl xl:max-w-7xl m-auto px-4 py-8">
+
+        </section>
+    )
+};
