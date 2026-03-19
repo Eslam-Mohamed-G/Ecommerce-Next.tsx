54bf46cf2b6fad4054fd03b89aa812a5618f9aaf
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2026-03-19 13:07:55 +0200

    add dynamic sitemap generation for SEO optimization

---
+++ src/app/sitemap.ts
@@ -0,0 +1,7 @@
+import { MetadataRoute } from "next";
+import productService from "../services/productService";
+
+// add dynamic sitemap generation for SEO optimization
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+
+};
